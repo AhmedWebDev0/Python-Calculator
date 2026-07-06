90398d91af133d8cf7c0deeab9be7348dae87c4f
Ahmed Mustafa <ahmed0abutabl@gmail.com>
2026-07-06 23:18:10 +0300

    Create Loop _ For.py

---
+++ Loop _ For.py
@@ -0,0 +1,24 @@
+mynumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in mynumber :
+
+    if number % 2 == 0 :
+    
+        print(f"The Number [ {number} ] Is Even")
+
+    else :
+
+        print(f"The Number [ {number} ] Is Odd")
+
+else :
+
+   print("The Loop I finished.")
+
+
+print("-" * 50)
+
+a = "Ahmed"
+
+for letter in a :
+
+    print(f"[ {letter.upper()} ]")
