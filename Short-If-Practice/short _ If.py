0be7082a5cfb292029d7c11eea130d4c8500ae2e
Ahmed Mustafa <ahmed0abutabl@gmail.com>
2026-07-15 22:53:46 +0300

    Implement age-based movie rating messages

---
+++ Short-If-Practice/short _ If.py
@@ -0,0 +1,17 @@
+movieRate =18
+age =int(("What/'s your age?"))
+
+if movieRate > age :
+    
+    print("Movie\'s Not Good 4U")
+
+elif movieRate < age :
+
+    print("Movie\'s Not Good 4U Happy Watching")
+
+else :
+
+    print("Movie\'s Not Good 4U Happy Watching")
+
+
+print("Movie\'s Not Good 4U" if movieRate > age  else "Movie\'s Not Good 4U Happy Watching")
